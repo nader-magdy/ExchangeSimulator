b44e16933afd41b83988cf927a15bf956027a78a
OShukri <o.shukri.sayed@gmail.com>
2016-10-10 22:24:20 +0200

    Creating new tests for Cancel Term page

--- test/pages/CancelTerm.page.ts
+++ test/pages/CancelTerm.page.ts
@@ -6,8 +6,34 @@ import { Address } from '../../src/app/entities/index'
 
 export class CancelTermPage extends BasePage {
 
+    h1HeaderTxt = element(by.css("#page-wrapper > div > ex-cancellation > div:nth-child(1) > div > h1"));
+    exchangeId_txt = element(by.css("#page-wrapper > div > ex-cancellation > div:nth-child(3) > div:nth-child(1) > div > div:nth-child(2) > ex-member > form > div > div > input"));
+    generated_X12 = element(by.css("#page-wrapper > div > ex-cancellation > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(2) > span"));
+    generated_X12_ID = element(by.css("#page-wrapper > div > ex-cancellation > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(2) > span > strong:nth-child(5)"));
+    generated_X12_reason = element(by.css("#page-wrapper > div > ex-cancellation > div:nth-child(3) > div:nth-child(2) > div > div:nth-child(2) > span > strong:nth-child(16)"));
+
     constructor() {
         super('cancellation');
     }
+
+    getH1HeaderText() {
+        return this.h1HeaderTxt.getText();
+    }
+    getexchangeId_txt() {
+        return this.exchangeId_txt.getText();
+    }
+    getgenerated_X12() {
+        return this.generated_X12.getText();
+    }
+    getgenerated_X12_ID() {
+        return this.generated_X12_ID.getText();
+    }
+    clickexchangeId_txt(){
+        return this.exchangeId_txt.click();
+    }
+
+    setexchangedId_text (test){
+        this.exchangeId_txt.sendKeys(test);
+    } 
 
 }
--- test/tests/Cancel-Term.spec.ts
+++ test/tests/Cancel-Term.spec.ts
@@ -15,3 +15,32 @@ beforeEach(() => {
     originalTimeout = jasmine.DEFAULT_TIMEOUT_INTERVAL;
     jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 });
+describe("check that header h1 text is Cancel/Term and title was printed", function () {
+    it("Print Title", function () {
+        var title = cancelpage.title();
+        title.then(function (value) {
+            console.log(value);
+        });
+    });
+    it("check H1 is Cancel/Term", function () {
+        cancelpage.getH1HeaderText().then(function (text) {
+            console.log(text);
+            expect(text).toContain("Cancel/Term");
+        });
+    });
+});
+describe("Check that the entered Exchange ID reflects on the generated X12", function () {
+    it("Enter ID", function () {
+        cancelpage.clickexchangeId_txt().then(function () {
+            cancelpage.setexchangedId_text('1234');
+            expect(cancelpage.getexchangeId_txt()).toContain('1234');
+        });
+    });
+    it("Check the ID in the generated X12", function () {
+        cancelpage.clickexchangeId_txt().then(function () {
+            cancelpage.setexchangedId_text('1234');
+            var text = cancelpage.getgenerated_X12_ID();
+            expect(text).toContain('1234');
+        });
+    });
+});
